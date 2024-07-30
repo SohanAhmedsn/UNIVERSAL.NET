0f1871b0bfa6508fcda31e49faa81f60a0cebcc4
SohanAhmedsn <sohan.ahmedself@gmail.com>
2024-07-30 22:38:10 +0530

    Update

---
+++ DATA.CONTEXT/ApplicationDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MODEL.EMPLOYEE.SECTION;
+
+
+namespace DATA.CONTEXT
+{
+    public class ApplicationDataContext : DbContext 
+    {
+       public ApplicationDataContext(DbContextOptions<ApplicationDataContext> option) : base(option) { }
+       public DbSet<Employee> Employee { get;set; } 
+       public DbSet<Department> Department { get;set; } 
+       public DbSet<Designation> Designation { get;set; } 
+
+    }
+}
---
+++ MODEL/EMPLOYEE.SECTION/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL.EMPLOYEE.SECTION
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string? FirstName {get;set;}
+        //public string? MiddleName {get;set;}
+        //public string? LastName {get;set;}
+        public DateTime DOB { get;set;}
+        #region Navigation
+
+
+        #endregion Navigation
+    }
+}
